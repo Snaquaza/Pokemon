9227bc715dd294c9fa59f79ebd61f6623ebf635d
Lucas Meijer <luxas.meijer@gmail.com>
2018-07-12 00:05:56 +0200

    Basic functions
    - Player character
    - Sprites for directions
    - Movement
    - Speed
    - Camera following

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject target;
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+	}
+}
