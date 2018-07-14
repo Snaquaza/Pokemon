2844bc2b731d79577fb312d0694974f80d4cd9b7
Lucas Meijer <luxas.meijer@gmail.com>
2018-07-14 23:10:44 +0200

    Better Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,108 +4,26 @@ using System.Collections;
 // Brainstorm Games youtube
 
 public class PlayerMovement : MonoBehaviour
-{
-
-    Direction currentDir;
-    Vector2 input;
-    bool isMoving = false;
-    Vector3 startPos;
-    Vector3 endPos;
-    float t;
-
-    public Sprite northSprite;
-    public Sprite eastSprite;
-    public Sprite southSprite;
-    public Sprite westSprite;
-
-    public float walkSpeed = 3f;
-
-    public bool isAllowedToMove = true;
+{ 
+	// Normal Movements Variables
+    private float walkSpeed;
+    private float curSpeed;
+    private float maxSpeed;
+	private Rigidbody2D rb;
 
     void Start()
-    {
-        isAllowedToMove = true;
+    {      
+        walkSpeed = 5;
+		rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        curSpeed = walkSpeed;
+        maxSpeed = curSpeed;
 
-        if (!isMoving && isAllowedToMove)
-        {
-            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
-                input.y = 0;
-            else
-                input.x = 0;
-
-            if (input != Vector2.zero)
-            {
-
-                if (input.x < 0)
-                {
-                    currentDir = Direction.West;
-                }
-                if (input.x > 0)
-                {
-                    currentDir = Direction.East;
-                }
-                if (input.y < 0)
-                {
-                    currentDir = Direction.South;
-                }
-                if (input.y > 0)
-                {
-                    currentDir = Direction.North;
-                }
-
-                switch (currentDir)
-                {
-                    case Direction.North:
-                        gameObject.GetComponent<SpriteRenderer>().sprite = northSprite;
-                        break;
-                    case Direction.East:
-                        gameObject.GetComponent<SpriteRenderer>().sprite = eastSprite;
-                        break;
-                    case Direction.South:
-                        gameObject.GetComponent<SpriteRenderer>().sprite = southSprite;
-                        break;
-                    case Direction.West:
-                        gameObject.GetComponent<SpriteRenderer>().sprite = westSprite;
-                        break;
-                }
-
-                StartCoroutine(Move(transform));
-            }
-
-        }
-
+        // Move senteces
+        rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
     }
-
-    public IEnumerator Move(Transform entity)
-    {
-        isMoving = true;
-        startPos = entity.position;
-        t = 0;
-
-        endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
-
-		if (!Physics2D.Raycast(startPos, endPos - startPos, 1))
-			while (t < 1f)
-			{
-				t += Time.deltaTime * walkSpeed;
-				entity.position = Vector3.Lerp(startPos, endPos, t);
-				yield return null;
-			}
-
-        isMoving = false;
-        yield return 0;
-    }
-}
-
-enum Direction
-{
-    North,
-    East,
-    South,
-    West
 }
